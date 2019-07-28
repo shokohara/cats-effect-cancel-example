298b0eed013472b9e486e9ebaec83e1dfbcc8620
shokohara <shokohara@users.noreply.github.com>
2019-07-28 11:21:00 +0900

    cancelBoundary not working

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,35 +1,27 @@
 package example
 
-import java.util.Scanner
-
 import cats.effect._
+import cats.effect.concurrent.MVar
 import cats.implicits._
-import scala.concurrent.duration._
 
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
 
 object Hello extends Greeting {
   def main(args: Array[String]): Unit = {
     implicit val timer: Timer[IO] = IO.timer(global)
     implicit val contextShift: ContextShift[IO] = IO.contextShift(global)
-    val fiber0 = fib(Int.MaxValue, 1, 1).start
-    val fiber = fiber0.unsafeRunSync()
-    println("Press Enter to Cancel")
-    val scanner = new Scanner(System.in)
-    scanner.nextLine()
+    val mVar = MVar.empty[IO, Boolean].unsafeRunSync()
+    val fiber = (fib(mVar).guaranteeCase(a => IO(println(a)))).start.unsafeRunSync()
+    println("started thread")
     fiber.cancel.unsafeRunSync()
+    println("requested cancel")
+    fiber.join.unsafeRunTimed(2000.millisecond)
+    println(mVar.tryTake.unsafeRunSync())
   }
 
-  def fib(n: Int, a: Long, b: Long)(implicit cs: ContextShift[IO], timer: Timer[IO]): IO[Long] =
-    IO.cancelBoundary *> IO.sleep(1.milliseconds) *> IO.cancelBoundary *> IO(()).flatMap(_ =>
-      if (n == 0) IO.pure(a) else {
-        println(n)
-        // Every 100-th cycle, check cancellation status
-        if (n % 100 == 0) {
-          //          IO.cancelBoundary *>
-          fib(n - 1, b, a + b)
-        } else fib(n - 1, b, a + b)
-      })
+  def fib(mVar: MVar[IO, Boolean])(implicit timer: Timer[IO], cs: ContextShift[IO]): IO[Unit] =
+    IO(Thread.sleep(1000)) *> IO.cancelBoundary *> mVar.put(true)
 }
 
 trait Greeting {
